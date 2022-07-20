7944a40a1fb45a6d45986a1ac2b414d7c6346339
Julie Han <juliemenghan@gmail.com>
2022-07-20 15:19:42 -0400

    finished searchShowbyTerms function

--- tvmaze.js
+++ tvmaze.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const $showsList = $("#showsList");
-const $episodesArea = $("#episodesArea");
-const $searchForm = $("#searchForm");
+const $showsList = $("#showsList"); //show area
+const $episodesArea = $("#episodesArea"); //episode area
+const $searchForm = $("#searchForm"); //form
+const TVMAZE_API = "https://api.tvmaze.com/search/shows";
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,27 +13,24 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+//param: searchTerm
+//grad ID: []
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function getShowsByTerm(searchTerm) {
+  // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const tvShowResults = await axios.get(TVMAZE_API, { params: { q: searchTerm } });
+
+  let tvShow = [];
+  for (let i = 0; i < tvShowResults.data.length; i++) {
+    tvShow.push({
+      id: tvShowResults.data[i].show.id,
+      name: tvShowResults.data[i].show.name,
+      image: tvShowResults.data[i].show.image.medium,
+      summary: tvShowResults.data[i].show.summary
+    });
+
+  }
+  return tvShow;
 }
 
 
@@ -42,31 +40,35 @@ function populateShows(shows) {
   $showsList.empty();
 
   for (let show of shows) {
-    const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
-         <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
-              class="w-25 me-3">
-           <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
-             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
-               Episodes
-             </button>
-           </div>
-         </div>  
-       </div>
-      `);
+    const $show =
 
-    $showsList.append($show);  }
+      // $(
+      //     `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      //      <div class="media">
+      //        <img
+      //           src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+      //           alt="Bletchly Circle San Francisco"
+      //           class="w-25 me-3">
+      //        <div class="media-body">
+      //          <h5 class="text-primary">${show.name}</h5>
+      //          <div><small>${show.summary}</small></div>
+      //          <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+      //            Episodes
+      //          </button>
+      //        </div>
+      //      </div>
+      //    </div>
+      //   `);
+
+      $showsList.append($show);
+  }
 }
 
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
+
 
 async function searchForShowAndDisplay() {
   const term = $("#searchForm-term").val();
